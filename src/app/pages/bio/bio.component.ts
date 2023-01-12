a6a092410458dc09d076eb4b4c5b08f12cfd41d5
Lexhoo <lexhoo@yahoo.fr>
2023-01-12 12:10:53 +0100

    Up navbar

--- src/app/pages/bio/bio.component.ts
+++ src/app/pages/bio/bio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { debounceTime, fromEvent, map, tap  } from 'rxjs';
 
 @Component({
   selector: 'app-bio',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./bio.component.css']
 })
 export class BioComponent {
+  name = 'Angular ' + VERSION.major;
 
+  showBtn$ = fromEvent(document, 'scroll').pipe(
+    debounceTime(50),
+    map(() => window.scrollY > 500),
+    tap(() => console.log('sas'))
+  );
+
+  // not Cross browsing (works on chrome - firefox)
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { debounceTime, fromEvent, map, tap  } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  name = 'Angular ' + VERSION.major;
 
+  showBtn$ = fromEvent(document, 'scroll').pipe(
+    debounceTime(50),
+    map(() => window.scrollY > 500),
+    tap(() => console.log('sas'))
+  );
+
+  // not Cross browsing (works on chrome - firefox)
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 }
